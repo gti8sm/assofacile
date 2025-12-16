3e4a81a0e0df9d8ce98b7d9c189e2b02ff340206
gti8sm <simonmaraval@gmail.com>
2025-12-16 14:26:16 +0100

    Protect /changelog behind authentication

--- app/Http/Controllers/ChangelogController.php
+++ app/Http/Controllers/ChangelogController.php
@@ -10,6 +10,10 @@ final class ChangelogController
 {
     public static function index(): void
     {
+        if (!isset($_SESSION['user_id'])) {
+            redirect('/login');
+        }
+
         $path = base_path('CHANGELOG.md');
         $content = is_file($path) ? (string)file_get_contents($path) : "# Changelog\n\nAucune version pour le moment.\n";
 
